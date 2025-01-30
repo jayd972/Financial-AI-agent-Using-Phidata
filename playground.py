206359353731cbc07cc8a2dc8d7d507870ee4810
Darji Jay <75724261+jayd972@users.noreply.github.com>
2025-01-29 22:25:07 -0800

    Add files via upload

---
+++ playground.py
@@ -0,0 +1,44 @@
+import openai
+import phi.api
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.model.openai import OpenAIChat
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+import os
+import phi
+from phi.playground import Playground, serve_playground_app 
+load_dotenv()
+
+phi.api = os.getenv("PHI_API_KEY")
+
+##Web search Agent
+web_search_agent = Agent(
+    name = "Web Search Agent",
+    role = "Search the web for the information",
+    model = Groq(id = "llama3-groq-70b-8192-tool-use-preview"),
+    tools =  [DuckDuckGo()],
+    instructions= ["Always include the source"],
+    show_tool_calls = True,
+    markdown = True,
+)
+
+##Financial Agent
+
+finance_agent = Agent(
+    name = "Finance AI Agent",
+    model = Groq(id = "llama3-groq-70b-8192-tool-use-preview"),
+    tools = [
+        YFinanceTools(stock_price = True, analyst_recommendations = True, stock_fundamentals = True, company_news = True) 
+    ],
+    instructions = ["Use Tables to display the data"],
+    show_tool_calls = True,
+    markdown = True
+)
+
+app = Playground(agents=[finance_agent,web_search_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
